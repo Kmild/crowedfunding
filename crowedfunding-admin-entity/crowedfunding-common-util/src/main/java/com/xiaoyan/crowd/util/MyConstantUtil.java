96304e46d8c1bd6c0f3a1320c89a9d8fb9987ef3
xiaoyan <aaa@dd.cmd>
2022-12-15 23:40:16 +0800

    four commit

--- crowedfunding-admin-entity/crowedfunding-common-util/src/main/java/com/xiaoyan/crowd/util/MyConstantUtil.java
+++ crowedfunding-admin-entity/crowedfunding-common-util/src/main/java/com/xiaoyan/crowd/util/MyConstantUtil.java
@@ -19,7 +19,7 @@ public class MyConstantUtil {
     public static final String MESSAGE_STRING_INVALIDATE="字符串不合法！请输入有效的字符串";
     public static final String MESSAGE_SYSTEM_ERROR_LOGIN_NOT_UNIQUE = "系统错误！用户名不唯一！";
     public static final String MESSAGE_SYSTEM_ERROR_LOGIN_PSW_IS_NULL = "系统错误！密码为空！";
-    public static final String MESSAGE_REMOVE_INVALID = "请勿进行非法删除！";
+    public static final String MESSAGE_REMOVE_INVALID = "hah请勿进行非法删除！";
     public static final String MESSAGE_ADD_DUPLICATEKRY ="抱歉！该账号名已经被使用过了！";
     public static final String MESSAGE_ACCESS_DENIED = "抱歉，您不能访问这个资源！";
     public static final String MESSAGE_CODE_NOT_CORRECT = "验证码无效！";
